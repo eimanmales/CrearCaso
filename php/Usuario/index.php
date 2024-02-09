1da2c3232fdd25c6929e51dbf2adf16270bcb9b0
Eiman Males <maleseiman@gmail.com>
2024-02-08 22:23:59 -0500

    UsuInvi

---
+++ php/Usuario/index.php
@@ -0,0 +1,13 @@
+<div class="contenedor">
+<button type="button" id="cancelar">Cancelar</button>
+    <div id="content">
+        <form action="login.php" method="post">
+            <input type="text" id="usuario" name="usuario" placeholder="Usuario" required>
+            <input type="password" id="password" name="password" placeholder="Clave" required>
+            <input type="submit" id="inicio" value="Inicio">
+
+        </form>
+        
+    </div>
+    
+</div>
